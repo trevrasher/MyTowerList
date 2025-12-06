a316ac0fc892fa5ba49b0d87b691873fe7ae9da1
trevrasher <93846352+trevrasher@users.noreply.github.com>
2025-12-05 17:16:01 -0800

    login button move

--- app/components/mainHeader.tsx
+++ app/components/mainHeader.tsx
@@ -2,7 +2,7 @@ import LoginButton from "./loginButton"
 import SyncButton from "./syncCompletions"
 
 interface MainHeaderProps {
-  isAuthenticated: boolean;
+    isAuthenticated: boolean;
 }
 
 export default function MainHeader({ isAuthenticated }: MainHeaderProps) {
@@ -10,12 +10,12 @@ export default function MainHeader({ isAuthenticated }: MainHeaderProps) {
         <div className="w-full h-15 gray-500 relative bg-zinc-900 p-2 mb-10">
             <div className="flex justify-center">
                 <span className="text-4xl mx-5">MyTowerList</span>
-                {isAuthenticated && <SyncButton/>}
-                <LoginButton/>
-                
-                
+                {isAuthenticated && <SyncButton />}
+                <div className="absolute top-2.5 right-14">
+                    <LoginButton />
+                </div>
             </div>
         </div>
-        
+
     )
 }
