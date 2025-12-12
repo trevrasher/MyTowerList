2e3719a260392a2fe9e9b5664578b71ea8ff172a
trevrasher <93846352+trevrasher@users.noreply.github.com>
2025-12-11 20:48:50 -0800

    creator serializer

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -2,18 +2,18 @@ from rest_framework import serializers
 from .models import Tower, Creator
 
 
-class TowerSerializer(serializers.ModelSerializer):
-    area = serializers.StringRelatedField()
-    creators = serializers.SerializerMethodField()
-    class Meta:
-        model = Tower
-        fields = '__all__'
-
-    def get_creators(self, obj):
-        return [creator.name for creator in obj.creators_m2m.all()]
-    
 
 class CreatorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Creator
         fields = ['name', 'roblox_user_id', 'avatar_url']
+        
+class TowerSerializer(serializers.ModelSerializer):
+    area = serializers.CharField(source='area.name', read_only=True)
+    creators = CreatorSerializer(source='creators_m2m', many=True, read_only=True)
+    
+    class Meta:
+        model = Tower
+        fields = '__all__'
+    
+
